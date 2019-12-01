ced63e07d1f6ae2d42e66b3a06fd17bbf492978f
Augustus <augustus@x-apps.com.br>
2019-12-01 16:00:00 -0300

    direction to gpx

--- README.md
+++ README.md
@@ -1,3 +1,91 @@
-# Strapi plugin strapi-plugin-gmaps-api-services
+<h1 align="center">Welcome to strapi-plugin-gmaps-api-services 👋</h1>
+<p>
+  <a href="https://www.npmjs.com/package/strapi-plugin-gmaps-api-services" target="_blank">
+    <img alt="Version" src="https://img.shields.io/npm/v/strapi-plugin-gmaps-api-services.svg">
+  </a>
+  <img src="https://img.shields.io/badge/node-%3E%3D10.0.0-blue.svg" />
+  <img src="https://img.shields.io/badge/npm-%3E%3D6.0.0-blue.svg" />
+  <a href="#" target="_blank">
+    <img alt="License: MIT" src="https://img.shields.io/badge/License-MIT-yellow.svg" />
+  </a>
+  <a href="https://twitter.com/augustuscosta" target="_blank">
+    <img alt="Twitter: augustuscosta" src="https://img.shields.io/twitter/follow/augustuscosta.svg?style=social" />
+  </a>
+</p>
 
-A quick description of strapi-plugin-gmaps-api-services.
+> Strapi plugin for use Google Maps API services.
+
+
+## Features
+
+- Directions service https://developers.google.com/maps/documentation/directions/intro
+- Directions to GPX using https://github.com/tyrasd/togpx ( just a first hand, needs improvement )
+- Distance Matrix service https://developers.google.com/maps/documentation/distance-matrix/start
+
+## Config
+
+- /config/custom.json
+
+
+```
+"googleMapsClient": {
+    "key": "",
+    "mode": {
+        "DRIVING": "driving",
+        "WALKING": "walking",
+        "BICYCLING": "bicycling",
+        "TRANSIT": "transit"
+    },
+    "avoid": {
+        "TOLLS": "tolls",
+        "HIGHWAYS": "highways",
+        "FERRIES": "ferries",
+        "INDOOR": "indoor"
+    },
+    "traffic_model": {
+        "BEST_GUESS": "best_guess",
+        "PESSIMISTIC": "pessimistic",
+        "OPTIMISTIC": "optimistic"
+    },
+    "transit_mode": {
+        "BUS": "bus",
+        "SUBWAY": "subway",
+        "TRAIN": "train",
+        "TRAM": "tram",
+        "RAIL": "rail"
+    },
+    "transit_routing_preference": {
+        "LESS_WALKING": "less_walking",
+        "FEWER_TRANSFERS": "fewer_transfers"
+    },
+    "units": "metric",
+    "language": "pt-BR",
+    "region": "BR",
+    "alternatives": false,
+    "optimize": false
+    }
+
+  ```
+
+
+## Prerequisites
+
+- node >=10.0.0
+- npm >=6.0.0
+
+```
+
+## Author
+
+👤 **Augustus Costa**
+
+* Twitter: [@augustuscosta](https://twitter.com/augustuscosta)
+* Github: [@augustuscosta](https://github.com/augustuscosta)
+
+## 🤝 Contributing
+
+Contributions, issues and feature requests are welcome!<br />Feel free to check [issues page](https://github.com/AugustusCosta/strapi-plugin-gmaps-api-services/issues).
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
--- services/GmapsApiServices.js
+++ services/GmapsApiServices.js
@@ -4,89 +4,104 @@
  * GmapsApiServices.js service
  *
  * @description: A set of functions similar to controller's actions to avoid code duplication.
+ * 
+ * Directions service docs https://developers.google.com/maps/documentation/directions/intro
+ * 
+ * Distance Matrix docs https://developers.google.com/maps/documentation/distance-matrix/start
  */
+
+const convertToGpx = points => {
+    const polyline = require('@mapbox/polyline');
+    const togpx = require('togpx');
+    const geojson = require('geojson');
+
+    const normalizedPoints = [];
+    const decodedPoints = polyline.decode(points);
+
+    for (let point of decodedPoints) {
+        const value = [point[1], point[0]];
+        normalizedPoints.push(value);
+    }
+
+    const data = { line: normalizedPoints };
+    const geoData = geojson.parse(data, { "LineString": "line" });
+
+    return togpx(geoData);
+};
 
 module.exports = {
 
-    directions: async ( origin, waypoints, destination ) =>
-    {
-        const googleMapsClient = require( '@google/maps' ).createClient( {
-            key: strapi.plugins[ 'gmaps-api-services' ].config.googleMapsClient.key,
+
+    directions: async (origin, waypoints, destination, getGpx = false) => {
+        const googleMapsClient = require('@google/maps').createClient({
+            key: strapi.plugins['gmaps-api-services'].config.googleMapsClient.key,
             Promise: Promise
-        } );
+        });
 
-        let result;
-        // waypoints.push(
-        //     {
-        //         latitude: -18.678388,
-        //         longitude: -46.626739
-        //     }
-        // );
-        // waypoints = [
-        //     {
-        //         latitude: -18.678388,
-        //         longitude: -46.626739
-        //     },
-        //     {
-        //         latitude: -17.109884,
-        //         longitude: -50.337268
-        //     },
-        // ];
+        let result, gpx;
 
-        result = await googleMapsClient.directions( {
+        //response format: https://developers.google.com/maps/documentation/directions/intro#json
+        result = await googleMapsClient.directions({
             origin,
             destination,
             waypoints,
-            mode: strapi.plugins[ 'gmaps-api-services' ].config.googleMapsClient.mode.DRIVING,
-            units: strapi.plugins[ 'gmaps-api-services' ].config.googleMapsClient.units,
-            language: strapi.plugins[ 'gmaps-api-services' ].config.googleMapsClient.language,
-            region: strapi.plugins[ 'gmaps-api-services' ].config.googleMapsClient.region,
-            alternatives: strapi.plugins[ 'gmaps-api-services' ].config.googleMapsClient.alternatives,
-            optimize: strapi.plugins[ 'gmaps-api-services' ].config.googleMapsClient.optimize,
-        } )
+            mode: strapi.plugins['gmaps-api-services'].config.googleMapsClient.mode.DRIVING,
+            units: strapi.plugins['gmaps-api-services'].config.googleMapsClient.units,
+            language: strapi.plugins['gmaps-api-services'].config.googleMapsClient.language,
+            region: strapi.plugins['gmaps-api-services'].config.googleMapsClient.region,
+            alternatives: strapi.plugins['gmaps-api-services'].config.googleMapsClient.alternatives,
+            optimize: strapi.plugins['gmaps-api-services'].config.googleMapsClient.optimize,
+        })
             .asPromise()
-            .then( ( response ) =>
-            {
+            .then((response) => {
                 return response.json;
-            } )
-            .catch( ( err ) =>
-            {
-                console.log( err );
+            })
+            .catch((err) => {
+                console.log(err);
                 return null;
-            } );
+            });
+        if (getGpx && result.routes && result.routes.length)
+            gpx = convertToGpx(result.routes[0].overview_polyline.points);
 
-        return { ...result, origin, destination, waypoints };
+        return { ...result, origin, destination, waypoints, gpx };
 
     },
-    distance: async ( origins = [], destinations = [] ) =>
-    {
-        const googleMapsClient = require( '@google/maps' ).createClient( {
-            key: strapi.plugins[ 'gmaps-api-services' ].config.googleMapsClient.key,
+
+
+
+
+    distance: async (origins = [], destinations = []) => {
+        const googleMapsClient = require('@google/maps').createClient({
+            key: strapi.plugins['gmaps-api-services'].config.googleMapsClient.key,
             Promise: Promise
-        } );
+        });
 
         let result;
 
-        result = await googleMapsClient.distanceMatrix( {
+        result = await googleMapsClient.distanceMatrix({
             origins,
             destinations,
-            mode: strapi.plugins[ 'gmaps-api-services' ].config.googleMapsClient.mode.DRIVING,
-            units: strapi.plugins[ 'gmaps-api-services' ].config.googleMapsClient.units,
-            language: strapi.plugins[ 'gmaps-api-services' ].config.googleMapsClient.language,
-            region: strapi.plugins[ 'gmaps-api-services' ].config.googleMapsClient.region
-        } )
+            mode: strapi.plugins['gmaps-api-services'].config.googleMapsClient.mode.DRIVING,
+            units: strapi.plugins['gmaps-api-services'].config.googleMapsClient.units,
+            language: strapi.plugins['gmaps-api-services'].config.googleMapsClient.language,
+            region: strapi.plugins['gmaps-api-services'].config.googleMapsClient.region
+        })
             .asPromise()
-            .then( ( response ) =>
-            {
+            .then((response) => {
                 return response.json;
-            } )
-            .catch( ( err ) =>
-            {
-                console.log( err );
+            })
+            .catch((err) => {
+                console.log(err);
                 return null;
-            } );
+            });
 
         return { ...result, origins, destinations };
 
     },
+
+
+    gpx: async overviewPolylinePoints => {
+        return convertToGpx(overviewPolylinePoints);
+    }
+
 };
